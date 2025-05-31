acdc3dcc5724198fdecd400f3469aa248edc53a8
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-05-31 21:55:13 +0300

    Add Participation Form

---
+++ app/Livewire/Components/ContactFormt.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Illuminate\Support\Facades\Mail;
+use Livewire\Component;
+
+class ContactFormt extends Component
+{
+
+    public $name;
+    public $email;
+    public $message;
+    public $sent = False;
+
+    public function render()
+    {
+        return view('livewire.components.contact-formt');
+    }
+
+    public function send() {
+
+        $email = 'tomas232@mail.ru';
+        Mail::to($email)->send(new \App\Mail\ContactForm($this->name, $this->email, $this->message));
+        $this->sent = True;
+    }
+}
