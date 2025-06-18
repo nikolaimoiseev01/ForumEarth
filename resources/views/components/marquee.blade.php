910af6f9b5d2cdcbe490e055180a0cc2ff52034d
Nikolai <nikolaimoiseev01@gmail.com>
2025-06-18 15:57:19 +0300

    Add Participation Form

--- resources/views/components/marquee.blade.php
+++ resources/views/components/marquee.blade.php
@@ -14,7 +14,7 @@
         class="relative w-full py-3 mx-auto overflow-hidden text-lg italic tracking-wide text-white uppercase sm:text-xs md:text-sm lg:text-base xl:text-xl 2xl:text-2xl">
         <div class="absolute left-0 z-20 w-40 h-full bg-gradient-to-r from-white to-transparent"></div>
         <div class="absolute right-0 z-20 w-40 h-full bg-gradient-to-l from-white to-transparent"></div>
-        <div x-ref="content" class="flex animate-marquee">
+        <div x-ref="content" class="flex">
             <div x-ref="item" class="flex items-center justify-around flex-shrink-0 w-full py-2 text-white">
                 @foreach($elements as $key=>$el)
                     <div
@@ -22,7 +22,7 @@
                         @if($countries == '0')px-16 py-8 max-h-32 bg-blue-300 @else flex-col @endif
                         rounded-3xl">
                         <img src="{{$el->getFirstMediaUrl('image')}}"
-                             class="@if($countries == '1') w-72 h-auto !max-w-72 !max-h-44 rounded-xl @else max-w-56 max-h-[inherit] @endif"
+                             class="@if($countries == '1') w-72 h-44 !max-w-72 !max-h-44 rounded-xl @else max-w-56 max-h-[inherit] @endif"
                              alt="">
                         @if($countries == '1')
                             <p class="text-blue-500">{{$el['name']}}</p>
