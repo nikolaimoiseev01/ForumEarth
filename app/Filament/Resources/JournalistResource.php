5374ee79c7333c23d26db01ba5c5b8c5c670cdbd
Nikolai <nikolaimoiseev01@gmail.com>
2025-06-20 00:15:46 +0300

    Add Participation Form

--- app/Filament/Resources/JournalistResource.php
+++ app/Filament/Resources/JournalistResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Exports\JournalistExporter;
+use App\Filament\Exports\ParticipantExporter;
 use App\Filament\Resources\JournalistResource\Pages;
 use App\Filament\Resources\JournalistResource\RelationManagers;
 use App\Models\Journalist;
@@ -9,6 +11,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\ExportAction;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -89,6 +92,11 @@ class JournalistResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
+            ])
+            ->headerActions([
+                ExportAction::make()
+                    ->exporter(JournalistExporter::class)
+                    ->label('Экспорт')
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
